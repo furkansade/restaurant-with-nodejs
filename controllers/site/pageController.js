1b60d3082867aa117e3d2f5f6763f98172d15e63
Furkan Sade Uckun <88970339+furkansade@users.noreply.github.com>
2023-10-04 09:32:40 +0300

    finished-home-page-routes

--- controllers/site/pageController.js
+++ controllers/site/pageController.js
@@ -1,7 +1,17 @@
 exports.getHomePage = async (req, res) => {
-  res.status(200).render("site/index");
+  res.status(200).render("site/index", {
+    pageName: "home",
+  });
 };
 
 exports.getAboutPage = async (req, res) => {
-  res.status(200).render("site/about");
+  res.status(200).render("site/about", {
+    pageName: "about",
+  });
+};
+
+exports.getMenuPage = async (req, res) => {
+  res.status(200).render("site/menu", {
+    pageName: "menu",
+  });
 };
--- routes/site/pageRoute.js
+++ routes/site/pageRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.route("/").get(pageController.getHomePage);
 router.route("/about").get(pageController.getAboutPage);
+router.route("/menu").get(pageController.getMenuPage)
 
 module.exports = router;
