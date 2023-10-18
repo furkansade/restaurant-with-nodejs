5e575f7338282ff81abe7f8f6aa0940c44b75a1a
Furkan Sade Uckun <88970339+furkansade@users.noreply.github.com>
2023-10-19 01:16:55 +0300

    customer-model-updated

--- models/Customer.js
+++ models/Customer.js
@@ -3,21 +3,116 @@ const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
-const CustomerSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  mail: {
+const CustomerSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
     type: String,
     required: true,
     unique: true,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  address: {
+    street: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    state: {
+      type: String,
+    },
+    postalCode: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
   },
   password: {
     type: String,
     required: true,
   },
-  phone: String,
-  orders: {
-    type: String,
+  permissions: {
+    admin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  orders: [
+    {
+      orderDate: {
+        type: Date,
+        required: true,
+      },
+      items: [
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          price: {
+            type: Number,
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+      totalAmount: {
+        type: Number,
+        required: true,
+      },
+      deliveryAddress: {
+        street: {
+          type: String,
+        },
+        city: {
+          type: String,
+        },
+        state: {
+          type: String,
+        },
+        postalCode: {
+          type: String,
+        },
+        country: {
+          type: String,
+        },
+      },
+      status: {
+        type: String,
+        enum: ["Preparing", "On the way", "Delivered", "Canceled"],
+        default: "Preparing",
+      },
+    },
+  ],
+  paymentMethods: [
+    {
+      cardType: {
+        type: String,
+      },
+      cardNumber: {
+        type: String,
+      },
+      expirationDate: {
+        type: String,
+      },
+    },
+  ],
+  registrationDate: {
+    type: Date,
+    default: Date.now,
   },
 });
 
